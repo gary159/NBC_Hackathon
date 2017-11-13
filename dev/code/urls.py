306e73c7a3e07222bd50306f5bf8ab5ac63a193f
SZTAJNMAN <gss2121@columbia.edu>
2017-11-13 10:49:17 -0500

    deleting duplicate

--- dev/code/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls import include, url
-from .views import PsBotView
-urlpatterns = [
-				url(r'^a5a7e87dd662e5ff2d5097d49cc06688384cc84c2e774633f5/?$', PsBotView.as_view()) 
-			]
